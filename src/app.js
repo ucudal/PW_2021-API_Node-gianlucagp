f4023c13d59536ac3fc8eca3dd5b8c281eca1e8b
gianlucagp <gianluca1998@hotmail.es>
2021-11-16 21:17:44 -0300

    endpoint get inicial

--- src/app.js
+++ src/app.js
@@ -1,13 +1,29 @@
-var express = require('express');
+const express = require('express');
+const app = express();
 
-var app = express();
+/*const experiencias = [
+  { "experiencia-laboral" :[ {"empresa":"cdargent","puesto":"interno","descripcion":"aburrido" , "fechaInicio": new Date("2020-07-15"), "fechaFin": new Date("2021-07-15")} ] }
+]*/
 
-app.get('/hola-mundo', function(req, res) {
+const experiencias = [
+  { "experiencia-laboral" :[ {"empresa":"cdargent","puesto":"interno","descripcion":"aburrido" , "fechaInicio": new Date("2020-07-15"), "fechaFin": new Date("2021-07-15")} ] }
+]
+
+app.get('/', function(req, res) {
   res.send("¡Hola mundo!")
+});
+
+app.get('/api/experiencia', function(req, res) {
+  res.send(experiencias)
+});
+
+app.get('/api/experiencia/:empresa', function(req, res) {
+  const experiencia = experiencias.find(c => c.empresa.localeCompare(req.params.empresa)===0);
+  if(!experiencia) res.status(404).send("404 no existe esa empresa");
+  res.send(experiencia);
 });
 
 app.listen(process.env.PORT || 3000, (a) => {
   console.log("Servidor disponible en http://localhost:3000")
 });
- 
-module.exports = app;
+
